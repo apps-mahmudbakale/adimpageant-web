ee45e1f7fd36cf479957e1fc62507551cd3642e0
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2024-07-07 09:43:41 +0100

    Wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
         </div>
     </div>
     <div class="relative hidden w-0 flex-1 lg:block">
-        <img class="absolute inset-0 h-full w-full object-cover" src="{{asset('login.png')}}" alt="">
+        <img class="absolute inset-0 h-full w-full object-cover" src="{{asset('public/login.png')}}" alt="">
     </div>
 </div>
 @endsection
