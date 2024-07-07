46c68fe38be45a4371df6ab2417faad49dfcde77
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2024-07-07 09:29:40 +0100

    Wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
     <titile>Adim Pageant | Landing Page</titile>
     <link rel="shortcut icon" href="{{asset('logo.png')}}" type="image/png">
     <style>
